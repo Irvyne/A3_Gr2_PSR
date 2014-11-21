ed930a65c0c46999c362af472b6723a24364a9f9
Thibaud BARDIN <thibaud.bardin@gmail.com>
2014-11-21 17:05:36 +0100

    Update

---
+++ tests/PasswordGeneratorTest.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Thibaud BARDIN (https://github.com/Irvyne).
+ * This code is under MIT licence (see https://github.com/Irvyne/license/blob/master/MIT.md)
+ */
+
+class PasswordGeneratorTest
+{
+
+}
